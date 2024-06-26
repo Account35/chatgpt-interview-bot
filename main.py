8f994efb98000bb4055506905fe1352a9a99936d
Account35 <127291893+Account35@users.noreply.github.com>
2024-06-26 11:39:25 +0200

    Third Changes

--- main.py
+++ main.py
@@ -18,13 +18,16 @@ async def post_audio(file: UploadFile):
 
 # function
 def transcribe_audio(file):
-    audio_file= open(file.filename, "rb")
-    transcription = open.audio.transcriptions.create( model="whisper-1", file=audio_file) 
+    #audio_file= open(file.filename, "rb")
+    #transcription = open.audio.transcriptions.create( model="whisper-1", file=audio_file) 
+    transcription = {"role": "user", "content": "Hello"}
     print(transcription)
     return{"message": "Audio has been transcribed"}
 
 def get_chat_response(user_message):
     messages = load_messages()
+    messages.append(user_message)
+
 
 def load_messages():
     messages = []
@@ -35,6 +38,16 @@ def load_messages():
     if not empty:
         with open(file) as db_file:
         data = json.load(db_file)
+        for item in data:
+            messages.append(item)
+        
+    else:
+        messages.append(
+            {"role": "system", "content": "You are  a searche engine assistant. Ask short question to get clear response. Your name is Greg. Keep responses 30minutes and be funny sometimes"}
+        )
+
+    return messages
+
 
 
    
